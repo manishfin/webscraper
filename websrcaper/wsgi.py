f9521b628923dcc9e4352e3fc92bc1ff7d9e5c72
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-04-09 01:06:44 +0530

    Rename

--- websrcaper/wsgi.py
+++ websrcaper/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for webscaper project.
+WSGI config for webscraper project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webscaper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webscraper.settings')
 
 application = get_wsgi_application()
